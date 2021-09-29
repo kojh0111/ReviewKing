e768286dc2fb089d0197c729156999422656294a
combiJihoon <combi_jihoon@naver.com>
2021-09-29 23:06:35 +0900

    Make models to create db using orm

--- backend/views/ranks.py
+++ backend/views/ranks.py
@@ -8,20 +8,7 @@ api = Api(ranks)
 # db = DB()
 class Ranks(Resource):
     def get(self):
-        con = get_connection()
-        # cusor : 커서 생성
-        cursor = con.cursor()
-        # sql문 작성(데이터 read)
-        sql = "SELECT * FROM reviews"
-        # sql문 실행(데이터 read)
-        cursor.execute(sql)
-        # fetchall() : 전부 가져오기
-        result = cursor.fetchall()
-        # commit 및 연결 해제
-        con.commit()  # 커밋은 반복할 필요 없음
-        con.close()
-
-        return jsonify(status="200", result=result)
+        return jsonify(status="200", result="success")
 
 
 api.add_resource(Ranks, "/ranks/")
