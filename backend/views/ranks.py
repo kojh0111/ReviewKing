e1bb55b23747e88132a7727520162ef7463e5ae7
combiJihoon <combi_jihoon@naver.com>
2021-10-01 13:45:58 +0900

    Backup
    Former-commit-id: cb5dff143a69493af7c7c8fe441f96802590ad9d

--- backend/views/ranks.py
+++ backend/views/ranks.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_restful import Resource, Api
-from db_connect import get_connection
+from models import Restaurants, Reviews
+from sqlalchemy.sql import func
+from app import db
 
 ranks = Blueprint("ranks", __name__)
 api = Api(ranks)
@@ -8,7 +10,25 @@ api = Api(ranks)
 # db = DB()
 class Ranks(Resource):
     def get(self):
-        return jsonify(status="200", result="success")
+        restaurant_cnt = (
+            db.session.query(func.count(Reviews.restaurant_id))
+            .group_by(Reviews.restaurant_id)
+            .all()
+        )
+
+        res = dict()
+        res["인허가된 서울시 음식점과 겹치는 크롤링 된 음식점 수"] = len(restaurant_cnt)
+        # for restaurant in restaurants:
+        #     tmp = dict()
+        #     tmp["name"] = restaurant.name
+        #     tmp["res_type"] = restaurant.res_type
+        #     tmp["total_rating"] = restaurant.total_rating
+        #     tmp["longitude_x"] = restaurant.longitude_x
+        #     tmp["latitude_y"] = restaurant.latitude_y
+
+        #     res["restaurant"] = tmp
+
+        return res, 200
 
 
-api.add_resource(Ranks, "/ranks/")
+api.add_resource(Ranks, "/ranks")
