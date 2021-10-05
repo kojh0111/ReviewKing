dd4e028d289fc5cf91a90c8a62db3d44f29a7493
combiJihoon <combi_jihoon@naver.com>
2021-10-05 11:29:04 +0900

    B: Change the query to select top3-ranked restaurants with the join query
    Former-commit-id: ff4aa29d018a750fb00273ce0923aa341ce1df36

--- backend/apis/what_to_eat.py
+++ backend/apis/what_to_eat.py
@@ -21,7 +21,8 @@ class WhatToEat(Resource):
         random_ids = random.sample(category_ids, 10)  # 무작위로 10개 뽑기
 
         randomly_selected_cats = [
-            Categories.query.filter_by(id=random_id).first().category for random_id in random_ids
+            Categories.query.filter_by(id=random_id).first().category
+            for random_id in random_ids
         ]
 
         img_urls = [
@@ -40,30 +41,29 @@ class WhatToEat(Resource):
         args = parser.parse_args()
         category = args["category"].strip()
 
-        # 해당 카테고리의 상위 평점 5개? 정도의 음식점 제공
-        restaurants = Restaurants.query.filter_by(category=category).all()
+        category_id = Categories.query.filter_by(category=category).first().id
 
-        # 상위 랭킹만 전달하기 위해 heap 사용
-        heap = []
-        for res in restaurants:
-            heapq.heappush(heap, (-(res.integrated_rating), res)) 
+        # 해당 카테고리의 상위 평점 3개의 음식점 제공
+        restaurants_rated = (
+            Restaurants.query.join(TotalRating)
+            .filter(Restaurants.category_id == category_id)
+            .order_by(TotalRating.integrated_rating.desc())[:4]
+        )
 
         data = dict()
-        # 상위 5개 가져오기
-        for _ in range(5):
-            res_rating, res = heapq.heappop(heap)
-            total_rating = TotalRating.query.filter_by(restaurant_id=res.id).first()
+        # 상위 3개 가져오기
+        for restaurant in restaurants_rated:
             tmp = {
-                "name": res.name,
-                "integrated_rating": -(res_rating),
-                "longitude": res.longitude_x,
-                "latitude": res.latitude_y
-                "naver": total_rating.naver,
-                "kakao": total_rating.kakao,
-                "mango": total_rating.mango,
-                "sikshin": total_rating.sikshin
+                "name": restaurant.name,
+                "integrated_rating": restaurant.integrated_rating,
+                "longitude": restaurant.longitude_x,
+                "latitude": restaurant.latitude_y,
+                "naver": restaurant.naver,
+                "kakao": restaurant.kakao,
+                "mango": restaurant.mango,
+                "siksin": restaurant.siksin,
             }
-            data[f'{res.name}'] = tmp
+            data[f"{res.name}"] = tmp
 
         return jsonify(status=200, data=data)
 
