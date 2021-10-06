3f70ab9f579516378a613e4882aba53208d53a2f
jh <kojh0111@naver.com>
2021-10-06 16:22:58 +0900

    B: review_restaurant review 수정
    Former-commit-id: 3ba42ea2c7d64edff5a1ab80ed10e1cb5079c739

--- backend/apis/review_restaurant.py
+++ backend/apis/review_restaurant.py
@@ -44,15 +44,14 @@ class ReviewRestaurant(Resource):
             total_rating = TotalRating.query.filter(
                 restaurant.id == TotalRating.restaurant_id
             ).first()
+            reviews = Reviews.query.filter(restaurant.id == Reviews.restaurant_id).all()
             tmp = {
                 "name": restaurant.name,
                 "naver": total_rating.naver,
                 "kakao": total_rating.kakao,
                 "mango": total_rating.mango,
                 "siksin": total_rating.siksin,
-                "reviews": [
-                    Reviews.query.filter(restaurant.id == Reviews.restaurant_id).all()
-                ],
+                "reviews": [review.content for review in reviews],
             }
             data[f"{restaurant.name}"] = tmp
 
