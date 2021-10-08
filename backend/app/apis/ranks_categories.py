a8240bd0b49a17219d6a3b93b4d49d7bcf463fdc
combiJihoon <combi_jihoon@naver.com>
2021-10-08 11:48:05 +0900

    B: Remove post method from ranks and change the class name from ranks to ranks_categories
    Former-commit-id: de26cea174e69a1eb5f448200eb4fbf0aa721582

---
+++ backend/app/apis/ranks_categories.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify
+from flask_restful import Resource, Api, reqparse
+from models import Restaurants, Categories, Reviews, TotalRating, Menus
+from sqlalchemy.sql import func
+from app import db
+
+ranks_categories = Blueprint("ranks_categories", __name__)
+api = Api(ranks)
+
+# request를 받기 위해서는 parser에 argument 추가 필요
+parser = reqparse.RequestParser()
+parser.add_argument("category", type=str)
+
+
+class RanksCategories(Resource):
+    # 음식 카테고리 제공
+    def get(self):
+        # 카테고리들 모두 불러오기
+        categories_data = Categories.query.all()
+
+        data = dict()
+        result = []
+        for category_data in categories_data:
+            restaurant = Restaurants.query.filter_by(
+                category_id=category_data.id
+            ).first()
+            tmp = {
+                "category_id": category_data.id,
+                "category": category_data.category,
+                "img_url": restaurant.img_url,
+            }
+            result.append(tmp)
+        data["result"] = result
+
+        return jsonify(status=200, data=data)
+
+
+api.add_resource(RanksCategories, "/ranks")
