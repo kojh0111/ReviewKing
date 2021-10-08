a9a2925fe4c1f174e13645c501a1cfa44325d29a
combiJihoon <combi_jihoon@naver.com>
2021-10-08 11:45:41 +0900

    B: Change the method from post to get
    Former-commit-id: a76f79671458d55611b59cb7686003875fdd50ec

---
+++ backend/app/apis/ranks_result.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, jsonify
+from flask_restful import Resource, Api, reqparse
+from models import Restaurants, Categories, Reviews, TotalRating, Menus
+from sqlalchemy.sql import func
+from app import db
+
+ranks_result = Blueprint("ranks_result", __name__)
+api = Api(ranks_result)
+
+# request를 받기 위해서는 parser에 argument 추가 필요
+parser = reqparse.RequestParser()
+parser.add_argument("category", type=str)
+
+
+class RanksResult(Resource):
+    def get(self, id=None):
+        # 해당 카테고리의 상위 평점 3개의 음식점 제공
+        restaurant = Restaurants.query.filter_by(category_id=id).first()
+        if restaurant:
+            restaurants_rated = (
+                Restaurants.query.join(TotalRating)
+                .filter(Restaurants.category_id == id)
+                .order_by(TotalRating.integrated_rating.desc())
+            )
+            data = dict()
+            rank = 1
+            result = []
+            for restaurant in restaurants_rated:
+                if rank == 4:
+                    break
+                menus = Menus.query.filter_by(restaurant_id=restaurant.id).all()
+                total_rating = TotalRating.query.filter_by(
+                    restaurant_id=restaurant.id
+                ).first()
+                tmp = {
+                    "name": restaurant.name,
+                    "integrated_rating": total_rating.integrated_rating,
+                    "img_url": restaurant.img_url,
+                    "menus": [menu.name for menu in menus],
+                    "rank": rank,
+                }
+                result.append(tmp)
+                rank += 1
+            data["result"] = result
+
+            return jsonify(status=200, data=data)
+        else:
+            return jsonify(status=404)
+
+
+api.add_resource(RanksResult, "/ranks/<int:id>")
