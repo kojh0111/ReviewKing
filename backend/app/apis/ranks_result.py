6f6fcda3c8731374d1f50783da783aed265a48c9
combiJihoon <combi_jihoon@naver.com>
2021-10-09 14:39:27 +0900

    B: Add category data in ranks_result
    Former-commit-id: 89422b1d3c274dbf02cfee458ba92edae138d8c9

--- backend/app/apis/ranks_result.py
+++ backend/app/apis/ranks_result.py
@@ -25,7 +25,7 @@ class RanksResult(Resource):
             )
 
             rank = 1
-            result = []
+            result = [category.category]
             for restaurant in restaurants_rated:
                 if rank == 4:
                     break
