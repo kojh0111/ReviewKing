ca101b6a970e766f1934a3ea1afc217c43461acc
Typonote <kimradium@naver.com>
2021-10-10 00:09:58 +0900

    F: 필요없는 파일 삭제
    Former-commit-id: 00b0d25a5e27683821a022eae5e616af3a7f8c77

--- frontend/src/const/chineseRestaurants.js
+++
@@ -1,5 +0,0 @@
-const ChineseRestaurants = ['북경짜장', '칭다오반점', '만리장성', '흑룡강성'];
-
-// TODO. 추후에 백엔드 API와 연결 시, 삭제 => 현재 테스트용으로 사용 중
-
-export default ChineseRestaurants;
--- frontend/src/const/westernRestaurants.js
+++
@@ -1,5 +0,0 @@
-const WesternRestaurants = ['맘마미아', '르블랑', '에그인헬', '몽블랑'];
-
-// TODO. 추후에 백엔드 API와 연결 시, 삭제 => 현재 테스트용으로 사용 중
-
-export default WesternRestaurants;
