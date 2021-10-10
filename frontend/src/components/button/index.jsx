f22cfb6beaf40486125c6e4d5250d9da9e624b84
jh <kojh0111@naver.com>
2021-10-10 21:37:21 +0900

    F: Introduce 페이지 내용 수정
    Former-commit-id: f260905d624446c0fb81d87b94df8a4ae0eeaead

--- frontend/src/components/button/index.jsx
+++ frontend/src/components/button/index.jsx
@@ -4,7 +4,7 @@ import './index.scss';
 
 export default function Button(props) {
   return (
-    <Link to="/what-to-eat/">
+    <Link to="/what-to-eat">
       <button type="button" className="ButtonWrapper" {...props}>
         {props.children}
       </button>
--- frontend/src/containers/introducepage/introduceContent.js
+++ frontend/src/containers/introducepage/introduceContent.js
@@ -1,15 +1,28 @@
 import './introduceContent.scss';
 import React from 'react';
-import Marginer from '../../components/marginer';
 import Members from '../../const/member';
 
 export default function IntroduceContent() {
   return (
     <div className="IntroduceContainer">
-      <Marginer direction="vertical" margin="3rem" />
-      <h2 className="TitleText"> 팀원 소개 예시</h2>
-      <Marginer direction="vertical" margin="1rem" />
 
+      <div className="ServiceIntroContainer">
+        <h2 className="TitleText"> 서비스 소개</h2>
+
+        <p className="Intro">
+          플랫폼 별 맛집 평점이 많이 차이나는 경우도 있다는 사실, 알고 계셨나요?
+        </p>
+        <p className="Intro">
+          우리는 어느 플랫폼을 믿어야 할까요?
+        </p>
+        <p className="Description">
+          <span>리뷰를 비교해보며</span>
+          <span>사용자에게 '어떤 플랫폼이 더 낫다'라고 알려주기보다는 플랫폼별로 비교를 할 수 있도록 합니다.</span>
+          <span>사용자는 더 좋은 선택을</span>
+          <span>이를 통해 합리적인 선택을 할 수 있게 될 것입니다.</span>
+        </p>
+      </div>
+      <h2 className="TitleText">팀원 소개</h2>
       <div className="PeopleIntroContainer">
         {Members.map(option => (
           <div className="CardContainer">
@@ -18,48 +31,6 @@ export default function IntroduceContent() {
             <h2 className="PeopleText">{option.role}</h2>
           </div>
         ))}
-      </div>
-
-      <div className="ServiceIntroContainer">
-        <Marginer direction="vertical" margin="4rem" />
-        <h2 className="TitleText"> 서비스 소개 예시</h2>
-        <Marginer direction="vertical" margin="2rem" />
-
-        <h3 className="bodyText">
-          소개 페이지는 리뷰왕 프로젝트를 함께하는 동료들을 소개하고 프로젝트의
-          시작 배경과 목표를 설명하는 페이지입니다.
-          <br />
-          <br />
-          동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화
-          삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          남산 위에 저 소나무 철갑을 두른 듯 바람 서리 불변함은 우리 기상일세
-          무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          가을 하늘 공활한데 높고 구름 없이 밝은 달은 우리 가슴 일편단심일세
-          무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라 사랑하세
-          무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          <br />
-        </h3>
-
-        <img alt="" className="IntroduceImage" />
-
-        <Marginer direction="vertical" margin="1rem" />
-
-        <h2 className="bodyText"> 서비스 사진 예시 입니다.</h2>
-
-        <Marginer direction="vertical" margin="1rem" />
-
-        <h3 className="bodyText">
-          동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화
-          삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          남산 위에 저 소나무 철갑을 두른 듯 바람 서리 불변함은 우리 기상일세
-          무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          가을 하늘 공활한데 높고 구름 없이 밝은 달은 우리 가슴 일편단심일세
-          무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라 사랑하세
-          무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 <br />
-          <br />
-        </h3>
       </div>
     </div>
   );
