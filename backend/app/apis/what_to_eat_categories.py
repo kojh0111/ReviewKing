41d9e96fad372e9ef046d38b95ee2c3f44e98fdf
combiJihoon <combi_jihoon@naver.com>
2021-10-11 18:55:33 +0900

    B: Add post method
    Former-commit-id: 13800ae5db697a331312ba73c823d7d535f22a4d

--- backend/app/apis/what_to_eat_categories.py
+++ backend/app/apis/what_to_eat_categories.py
@@ -1,15 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_restful import Resource, Api, reqparse
 from models import Restaurants, Categories, Reviews, TotalRating, Menus
 from app import db
 import random
+import heapq
 
 eat_categories = Blueprint("eat-categories", __name__)
 api = Api(eat_categories)
 
 # request를 받기 위해서는 parser에 argument 추가 필요
 parser = reqparse.RequestParser()
-parser.add_argument("subcategory", type=str, location="json")
+parser.add_argument("category", type=str, location="json", action="append")
 
 
 class WhatToEatCategories(Resource):
@@ -24,20 +25,43 @@ class WhatToEatCategories(Resource):
             category = Categories.query.filter_by(id=random_id).first()
             tmp = {
                 "category_id": category.id,
-                "category": category.subcategory,
+                "category": category.category,
+                "subcategory": category.subcategory,
                 "img_url": restaurant.img_url,
             }
             result.append(tmp)
 
         return jsonify(status=200, data=result)
 
-    # def post(self):
-    #     args = parser.parse_args()
-    #     subcategories = args["subcategory"]
+    def post(self):
+        args = parser.parse_args()
 
-    #     categories = Categories.query.all()
-    #     category_ids = [cat.id for cat in categories]
-    #     random_ids = random.sample(category_ids, 10)  # 무작위로 10개 뽑기
+        categories = args["category"]
+        return
+        # categories_data = Categories.query.filter(Categories.id.in_(categories)).all()
+
+        # heap = []
+        # for category in categories_data:
+        #     restaurant = (
+        #         Restaurants.query.filter_by(category_id=category.id)
+        #         .order_by(Restaurants.integrated_rating.desc())
+        #         .first()
+        #     )
+        #     heapq.heappush(heap, (restaurant.integrated_rating, restaurant))
+
+        # result = []
+        # while heap:
+        #     integrated_rating, restaurant = heapq.happop(heap)
+        #     tmp = {
+        #         "name": restaurant.name,
+        #         "restaurnt_id": restaurant.id,
+        #         "integrated_rating": round(float(total_rating.integrated_rating), 2),
+        #         "rank": rank,
+        #     }
+        #     result.append(tmp)
+
+        # return jsonify(status=200, data=result)
+        return
 
 
 api.add_resource(WhatToEatCategories, "/what-to-eat")
