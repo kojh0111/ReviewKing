8788cd49f9ae2396f3e5bad25f6aa08898ac3ab7
combiJihoon <combi_jihoon@naver.com>
2021-10-12 11:50:27 +0900

    B: Change the name of the module
    Former-commit-id: a7f5766cfcc34c09a66762b6d5dcbed205c6e8ed

---
+++ backend/app/apis/restaurants_all.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, jsonify
+from flask_restful import Resource, Api, reqparse
+from models import Restaurants, Categories
+
+all_restaurants = Blueprint("all-restaurant", __name__)
+api = Api(all_restaurants)
+
+
+class AllRestaurants(Resource):
+    # 음식점 검색창 제공
+    def get(self):
+        # 지도 제공
+        restaurant_data = Restaurants.query.all()
+        data = list()
+        for restaurant in restaurant_data:
+            category = Categories.query.filter(
+                Categories.id == restaurant.category_id
+            ).first()
+            tmp = {
+                "name": restaurant.name,
+                "lat": restaurant.latitude_y,
+                "lng": restaurant.longitude_x,
+                "img": restaurant.img_url,
+                "category": category.category,
+                "id": restaurant.id,
+            }
+            data.append(tmp)
+
+        return jsonify(status=200, restaurants=data)
+
+
+api.add_resource(AllRestaurants, "/reviews")
