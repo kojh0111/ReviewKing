02131e6087c64b6b0f0d470efce876d94cb2d976
combiJihoon <combi_jihoon@naver.com>
2021-10-12 11:46:34 +0900

    B: Delete unnecessary models codes
    Former-commit-id: 053a82ad73d70a7143c456945e55011ea477f189

--- backend/app/apis/ranks_categories.py
+++ backend/app/apis/ranks_categories.py
@@ -1,21 +1,15 @@
 from flask import Blueprint, jsonify
 from flask_restful import Resource, Api, reqparse
-from models import Restaurants, Categories, Reviews, TotalRating, Menus
+from models import Restaurants, Categories, TotalRating
 from sqlalchemy.sql import func
 from app import db
 
 ranks_categories = Blueprint("ranks_categories", __name__)
 api = Api(ranks_categories)
 
-# request를 받기 위해서는 parser에 argument 추가 필요
-parser = reqparse.RequestParser()
-parser.add_argument("category", type=str)
-
 
 class RanksCategories(Resource):
-    # 음식 카테고리 제공
     def get(self):
-        # 카테고리들 모두 불러오기
         categories_data = Categories.query.all()
 
         result = []
@@ -23,10 +17,6 @@ class RanksCategories(Resource):
             restaurant = Restaurants.query.filter_by(
                 category_id=category_data.id
             ).first()
-            if restaurant:
-                img_url = restaurant.img_url
-            else:
-                img_url = None
             tmp = {
                 "category_id": category_data.id,
                 "category": category_data.category,
