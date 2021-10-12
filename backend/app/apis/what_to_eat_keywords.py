d3ddea1d14a2a80d4acd6ae06c06bb307f83c1b4
combiJihoon <combi_jihoon@naver.com>
2021-10-12 10:42:30 +0900

    B: Change the api to send the data with keywords included
    Former-commit-id: 648fc29e4f75385bc9992f2a11b65c5da2dc4b55

---
+++ backend/app/apis/what_to_eat_keywords.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify
+from flask_restful import Resource, Api, reqparse
+from models import Keywords
+from app import db
+
+what_to_eat_keywords = Blueprint("what-to-eat-keywords", __name__)
+api = Api(what_to_eat_keywords)
+
+# request를 받기 위해서는 parser에 argument 추가 필요
+parser = reqparse.RequestParser()
+parser.add_argument("subcategory", type=str)
+
+
+class WhatToEatKeywords(Resource):
+    def post(self, subcategory=None):
+        keywords_data = Keywords.query.filter_by(subcategory=subcategory).all()
+
+        if keywords_data:
+            keywords = [data.keyword for data in keywords_data]
+
+            return jsonify(status=200, keywords=sorted(keywords))
+        else:
+            return jsonify(status=404)
+
+
+api.add_resource(WhatToEatResult, "/what-to-eat/<str:subcategory>")
