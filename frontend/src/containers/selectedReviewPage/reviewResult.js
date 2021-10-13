4936144371768f017a17a06a067ccef5d5bc94e8
combiJihoon <combi_jihoon@naver.com>
2021-10-13 23:32:12 +0900

    Change the color of the selectedText to deepblue
    Former-commit-id: c153bd2e9c367b544fc77b572f147a698937ca31

--- frontend/src/containers/selectedReviewPage/reviewResult.js
+++ frontend/src/containers/selectedReviewPage/reviewResult.js
@@ -59,9 +59,7 @@ export default function ReviewResult() {
       <div className="ReviewIntroContainer">
         <Marginer direction="vertical" margin="2rem" />
         <div className="ReviewNameContainer">
-          <h2 className="SelectedText" style={{ color: '#ff5722' }}>
-            {reviewResult.name}
-          </h2>
+          <h2 className="SelectedText">{reviewResult.name}</h2>
           <h2 className="NumberText" style={{ color: '#0b214a' }}>
             종합평점 : {reviewResult.integrated_rating}
           </h2>
