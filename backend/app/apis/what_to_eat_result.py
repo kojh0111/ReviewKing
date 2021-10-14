2ae5d33bb81fa42504ffdf0376eb030e8555dd4e
jh <kojh0111@naver.com>
2021-10-14 15:11:22 +0900

    B: what to eat api 빈도수 기반 추천(2)
    Former-commit-id: b50a9faaadde4ec7b73bdf7d88ff734893fe60aa

--- backend/app/apis/what_to_eat_result.py
+++ backend/app/apis/what_to_eat_result.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from models import Keywords, KeyResLink, TotalRating, Restaurants
 from sqlalchemy import and_
 from collections import Counter
-
+import heapq
 
 what_to_eat_result = Blueprint("what-to-eat-result", __name__)
 api = Api(what_to_eat_result)
@@ -20,7 +20,54 @@ class WhatToEatResult(Resource):
         args = parser.parse_args()
         keywords = args["key"]
 
-        if len(keywords) != 0:
+        if len(keywords) == 1:
+            # 해당 keyword를 가진 음식점들 중 평점 높은 것들 heap에 저장
+            heap = []
+            for keyword in keywords:
+                key_data = Keywords.query.filter(
+                    and_(
+                        Keywords.keyword == keyword, Keywords.subcategory == subcategory
+                    )
+                ).first()
+                key_res_links = KeyResLink.query.filter_by(keyword_id=key_data.id).all()
+                if key_res_links:
+                    for key_res_link in key_res_links:
+                        res_id = key_res_link.restaurant_id
+                        restaurant = Restaurants.query.filter_by(id=res_id).first()
+                        total_rating = TotalRating.query.filter_by(
+                            restaurant_id=res_id
+                        ).first()
+                        if (-total_rating.integrated_rating, restaurant) not in heap:
+                            heapq.heappush(
+                                heap, (-total_rating.integrated_rating, restaurant)
+                            )
+                        else:
+                            pass
+
+            # 상위 3개 가져오기
+            rank = 1
+            result = []
+            while heap and rank <= 3:
+                integrated_rating, restaurant = heapq.heappop(heap)
+                total_rating = TotalRating.query.filter_by(
+                    restaurant_id=restaurant.id
+                ).first()
+                tmp = {
+                    "name": restaurant.name,
+                    "id": restaurant.id,
+                    "naver": total_rating.naver,
+                    "kakao": total_rating.kakao,
+                    "mango": total_rating.mango,
+                    "siksin": total_rating.siksin,
+                    "integrated_rating": round(float(-integrated_rating), 2),
+                    "rank": rank,
+                }
+                result.append(tmp)
+                rank += 1
+
+            return jsonify(status=200, result=result)
+
+        elif len(keywords) != 0:
             for keyword in keywords:
                 key_data = Keywords.query.filter(
                     and_(
@@ -33,18 +80,25 @@ class WhatToEatResult(Resource):
                     for key_res_link in key_res_links:
                         res_id = key_res_link.restaurant_id
                         restaurant = Restaurants.query.filter_by(id=res_id).first()
-                        res_list.append(restaurant.id)
-                    final_list = [x for x, _ in Counter(res_list).most_common(3)]
+                        total_rating = TotalRating.query.filter_by(
+                            restaurant_id=res_id
+                        ).first()
+                        heapq.heappush(
+                            res_list, (-total_rating.integrated_rating, restaurant.id)
+                        )
+                    final_res = [x for x, _ in Counter(res_list).most_common(3)]
 
             # 상위 3개 가져오기
             rank = 1
             result = []
-            for most_res_id in final_list:
+            for most_res_id in final_res:
                 total_rating = TotalRating.query.filter_by(
                     restaurant_id=most_res_id
                 ).first()
+                srestaurant = Restaurants.query.filter_by(id=most_res_id).first()
                 tmp = {
-                    "name": restaurant.name,
+                    "name": srestaurant.name,
+                    "id": most_res_id,
                     "naver": total_rating.naver,
                     "kakao": total_rating.kakao,
                     "mango": total_rating.mango,
