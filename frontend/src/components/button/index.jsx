599696935450e7314d1c1ad1cb17eb8215feb9ca
Typonote <kimradium@naver.com>
2021-10-15 13:13:10 +0900

    f: 충돌 해결
    Former-commit-id: 210105fc96a9fc62ed8509e9dc909f0b500aefec

--- frontend/src/components/button/index.jsx
+++ frontend/src/components/button/index.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { scroller } from 'react-scroll';
 import './index.scss';
 
 export default function Button(props) {
   const scrollToServiceSection = () => {
-<<<<<<< HEAD
-    scroller.scrollTo('ServicePageContainer', { smooth: true, duration: 1500 });
-  };
-
-  return (
-    <Link to="/reviews" onClick={scrollToServiceSection}>
-      <div type="button" className="ButtonWrapper" {...props}>
-=======
     window.scrollTo({
       top: document.documentElement.clientHeight,
       behavior: 'smooth',
@@ -21,7 +12,6 @@ export default function Button(props) {
   return (
     <Link to="/what-to-eat" onClick={scrollToServiceSection}>
       <div className="ButtonWrapper" {...props}>
->>>>>>> ccd512c58b85d96c2b4b5928fad279df40de9a83
         {props.children}
       </div>
     </Link>
